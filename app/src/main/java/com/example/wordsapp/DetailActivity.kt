17a3f1fdf954f2c4e4b5f03c062c01ef05484c90
Ignacio Gonzalez <mylaneza@gmail.com>
2022-08-31 22:24:57 -0500

    Implement LetterListFragment

--- app/src/main/java/com/example/wordsapp/DetailActivity.kt
+++ app/src/main/java/com/example/wordsapp/DetailActivity.kt
@@ -24,10 +24,7 @@ import com.example.wordsapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
-    companion object{
-        const val LETTER = "letter"
-        const val SEARCH_PREFIX = "https://www.google.com/search?q="
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,16 +37,7 @@ class DetailActivity : AppCompatActivity() {
         // Retrieve the LETTER from the Intent extras
         // intent.extras.getString returns String? (String or null)
         // so toString() guarantees that the value will be a String
-        val letterId = intent?.extras?.getString(LETTER).toString()
-        val recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = WordAdapter(letterId, this)
 
-        // Adds a [DividerItemDecoration] between items
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        )
 
-        title = getString(R.string.detail_prefix) + " " + letterId
     }
 }
